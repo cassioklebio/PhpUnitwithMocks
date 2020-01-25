635b2dec0e39e700680ddd574be6dc93a0de6064
cassio klebio <cassiotr2009@gmail.com>
2020-01-25 11:11:01 -0300

    Conhecendo Mocks e Stubs

--- tests/Service/EncerradorTest.php
+++ tests/Service/EncerradorTest.php
@@ -8,53 +8,58 @@ use Alura\Leilao\Model\Leilao;
 use Alura\Leilao\Service\Encerrador;
 use PHPUnit\Framework\TestCase;
 
-/**
- * Class LeilaoDaoMock
- * @package Alura\Leilao\Tests\Service Mock
- */
-class LeilaoDaoMock extends LeilaoDao
-{
-    /**
-     * @var array $leiloes que reciberá os leilões
-     */
-    private $leiloes = [];
-
-    /**
-     * Metodo Mock para salvar
-     * @param Leilao $leilao
-     */
-  public function salva(Leilao $leilao): void
-  {
-      $this->leiloes[] = $leilao;
-  }
-
-    /**
-     * Metodo Mock recuperarNaoFinalizados
-     * @return array
-     */
-  public function recuperarNaoFinalizados(): array
-  {
-      return array_filter($this->leiloes, function (Leilao $leilao){
-          return !$leilao->estaFinalizado();
-      });
-  }
-
-  public function recuperarFinalizados(): array
-  {
-      return array_filter($this->leiloes, function (Leilao $leilao){
-          return $leilao->estaFinalizado();
-      });
-  }
-
-    /**
-     * Metodo Mock atualiza
-     * @param Leilao $leilao
-     */
-  public function atualiza(Leilao $leilao)
-  {
-
-  }
-}
+///**
+// * Class LeilaoDaoMock
+// * @package Alura\Leilao\Tests\Service Mock
+// */
+//class LeilaoDaoMock extends LeilaoDao
+//{
+//    /**
+//     * @var array $leiloes que reciberá os leilões
+//     */
+//    private $leiloes = [];
+//
+//    /**
+//     * Metodo Mock para salvar
+//     * @param Leilao $leilao
+//     */
+//  public function salva(Leilao $leilao): void
+//  {
+//      $this->leiloes[] = $leilao;
+//  }
+//
+//  /**
+//   * Metodo Mock recuperarNaoFinalizados
+//   * @return array
+//   */
+//  public function recuperarNaoFinalizados(): array
+//  {
+//      return array_filter($this->leiloes, function (Leilao $leilao){
+//          return !$leilao->estaFinalizado();
+//      });
+//  }
+//
+//    /**
+//     * Metodo Mock recuperarFinalizados
+//     *
+//     * @return array
+//     */
+//  public function recuperarFinalizados(): array
+//  {
+//      return array_filter($this->leiloes, function (Leilao $leilao){
+//          return $leilao->estaFinalizado();
+//      });
+//  }
+//
+//    /**
+//     * Metodo Mock atualiza
+//     * @param Leilao $leilao
+//     */
+//  public function atualiza(Leilao $leilao)
+//  {
+//
+//  }
+//}
 
 /**
  * Class EncerradorTest
@@ -62,8 +67,10 @@ class LeilaoDaoMock extends LeilaoDao
  */
 class EncerradorTest extends TestCase
 {
-
-
+    /**
+     * Metodo para testar Leiloes com mais de uma semana devem ser encerrados
+     *
+     */
     public function testLeiLoesComMaisDeUmaSemanaDeVemSerEncerrador()
     {
         $fiat147 = new Leilao(
@@ -78,21 +85,34 @@ class EncerradorTest extends TestCase
 
 
 
-        $leilaoDao = new LeilaoDaoMock();
-        $leilaoDao->salva($fiat147);
-        $leilaoDao->salva($variant);
+        $leilaoDao = $this->createMock(LeilaoDao::class);
+//        $leilaoDao = $this->getMockBuilder(LeilaoDao::class)
+//            ->setConstructorArgs([new \PDO('sqlite::memory:')])
+//            ->getMock();
+
+        $leilaoDao->method('recuperarNaoFinalizados')
+                  ->willReturn([$fiat147, $variant]);
+        $leilaoDao->method('recuperarFinalizados')
+            ->willReturn([$fiat147, $variant]);
+        $leilaoDao->expects($this->exactly(2))
+            ->method('atualiza')
+            ->withConsecutive(
+                [$fiat147],
+                [$variant]
+            );
+
 
         $encerrador = new Encerrador($leilaoDao);
         $encerrador->encerra();
 
         //Assert
-        $leiloes = $leilaoDao->recuperarFinalizados();
-        self::assertCount(2, $leiloes);
-        self::assertEquals(
-            'Fiat 147 0km',
-            $leiloes[0]->recuperarDescricao());
 
-        self::assertEquals('Variant 1972 0km', $leiloes[1]->recuperarDescricao());
+        $leiloes = [$fiat147,$variant];
+        self::assertCount(2, $leiloes);
+        self::assertTrue($leiloes[0]->estaFinalizado());
+        self::assertTrue($leiloes[1]->estaFinalizado());
+
+
 
     }
 }
